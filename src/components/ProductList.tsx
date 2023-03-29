75c8c970822193f00e3388c033e60dd793718e08
Stone Comstock <comstockstone@yahoo.com>
2023-03-29 14:44:26 +0700

    use Products custom hook

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,6 +1,7 @@
+import useCart from '../hooks/useCart';
+import useProducts from '../hooks/useProducts';
+
 const ProductList = () => {
-  return (
-    <div>ProductList</div>
-  )
-}
-export default ProductList
+  return <div>ProductList</div>;
+};
+export default ProductList;
